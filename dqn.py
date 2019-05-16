e6cbecdf81b1b4221511ea919903e40f998d00fe
Ritchie <ritchie_w@outlook.com>
2019-05-15 23:34:04 -0400

    adjust network structure

--- dqn.py
+++ dqn.py
@@ -9,12 +9,14 @@ class DQN(nn.Module):
     def __init__(self, n_features=8):
         super(DQN, self).__init__()
         self.fc1 = nn.Linear(n_features, 16)
-        self.activation1 = nn.Sigmoid()
         self.fc2 = nn.Linear(16, 8)
-        self.activation2 = nn.Sigmoid()
-        self.dropout = nn.Dropout(.2)
         self.fc3 = nn.Linear(8, 1)
+        self.dropout = nn.Dropout(.2)
+        self.activation = nn.Sigmoid()
         self.criterion = nn.MSELoss()
+        nn.init.uniform_(self.fc1.weight)
+        nn.init.uniform_(self.fc2.weight)
+        nn.init.uniform_(self.fc3.weight)
     
     @staticmethod
     def dict2vec(counter):
@@ -25,12 +27,10 @@ class DQN(nn.Module):
         return np.array(features).astype(np.float32)
 
     def forward(self, x):
-        if isinstance(x, np.ndarray):
-            x = torch.from_numpy(x)
         x = self.fc1(x)
-        x = self.activation1(x)
+        x = self.activation(x)
         x = self.fc2(x)
-        x = self.activation2(x)
+        x = self.activation(x)
         # x = self.dropout(x)
         o = self.fc3(x)
         return o
