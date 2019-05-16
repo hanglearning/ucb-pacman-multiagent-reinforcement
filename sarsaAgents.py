007eab55892e651698a8ae17c2ff2c631b468fc3
Ritchie <ritchie_w@outlook.com>
2019-05-15 23:33:48 -0400

    remove redundant class

--- sarsaAgents.py
+++ sarsaAgents.py
@@ -22,62 +22,29 @@ from learningAgents import ReinforcementAgent
 from featureExtractors import *
 
 
-class SarsaAgent(ReinforcementAgent):
-    """
-      Sarsa Agent
-
-      Functions you should fill in:
-        - computeValueFromQValues
-        - computeActionFromQValues
-        - getQValue
-        - getAction
-        - update
-
-      Instance variables you have access to
-        - self.epsilon (exploration prob)
-        - self.alpha (learning rate)
-        - self.discount (discount rate)
-
-      Functions you should use
-        - self.getLegalActions(state)
-          which returns legal actions for a state
-    """
-    def __init__(self, **args):
-        "You can initialize Q-values here..."
+class PacmanSarsaAgent(ReinforcementAgent):
+    def __init__(self, epsilon=0.05, gamma=0.8, alpha=0.2, numTraining=0, **args):
+        args['epsilon'] = epsilon
+        args['gamma'] = gamma
+        args['alpha'] = alpha
+        args['numTraining'] = numTraining
+        self.index = 0  # This is always Pacman
         ReinforcementAgent.__init__(self, **args)
 
-        self.q_table = {}
 
-    def getQValue(self, state, action):
-        """
-          Returns Q(state,action)
-          Should return 0.0 if we have never seen a state
-          or the Q node value otherwise
-        """
-        if state not in self.q_table.keys():
-            self.q_table[state] = {}
-        if action not in self.q_table[state].keys():
-            self.q_table[state][action] = 0.
-        return self.q_table[state][action]
+class ApproximateSarsaAgent(PacmanSarsaAgent):
+    def __init__(self, extractor='ComplexExtractor', **args):
+        self.featExtractor = util.lookup(extractor, globals())()
+        self.weights = util.Counter()
+        PacmanSarsaAgent.__init__(self, **args)
 
     def computeValueFromQValues(self, state):
-        """
-          Returns max_action Q(state,action)
-          where the max is over legal actions.  Note that if
-          there are no legal actions, which is the case at the
-          terminal state, you should return a value of 0.0.
-        """
         legalActions = self.getLegalActions(state)
         if legalActions is None:
             return 0.
         return max([self.getQValue(state, action) for action in legalActions])
 
     def computeActionFromQValues(self, state):
-        """
-          Compute the best action to take in a state.  Note that if there
-          are no legal actions, which is the case at the terminal state,
-          you should return None.
-        """
         legalActions = self.getLegalActions(state)
         action = None
         if legalActions is None or len(legalActions) == 0:
@@ -92,17 +59,6 @@ class SarsaAgent(ReinforcementAgent):
         return action
 
     def getAction(self, state):
-        """
-          Compute the action to take in the current state.  With
-          probability self.epsilon, we should take a random action and
-          take the best policy action otherwise.  Note that if there are
-          no legal actions, which is the case at the terminal state, you
-          should choose None as the action.
-
-          HINT: You might want to use util.flipCoin(prob)
-          HINT: To pick randomly from a list, use random.choice(list)
-        """
-        # Pick Action
         legalActions = self.getLegalActions(state)
         action = None
         if legalActions is None or len(legalActions) == 0:
@@ -111,92 +67,22 @@ class SarsaAgent(ReinforcementAgent):
             action = random.choice(legalActions)
         else:
             action = self.getPolicy(state)
+        self.doAction(state, action)
         return action
-
-    def update(self, state, action, nextState, reward):
-        """
-          The parent class calls this to observe a
-          state = action => nextState and reward transition.
-          You should do your Q-Value update here
-
-          NOTE: You should never call this function,
-          it will be called on your behalf
-        """
-        nextAction = self.getAction(nextState)
-        newQ = (1-self.alpha)*self.getQValue(state, action)+self.alpha*(reward+self.discount*self.getQValue(nextState, nextAction))
-        self.setQValue(state, action, newQ)
 
     def getPolicy(self, state):
         return self.computeActionFromQValues(state)
 
     def getValue(self, state):
         return self.computeValueFromQValues(state)
-    
-    def setQValue(self, state, action, qvalue):
-        self.q_table[state][action] = qvalue
-
-
-class PacmanSarsaAgent(SarsaAgent):
-    "Exactly the same as SarsaAgent, but with different default parameters"
-
-    def __init__(self, epsilon=0.05, gamma=0.8, alpha=0.2, numTraining=0, **args):
-        """
-        These default parameters can be changed from the pacman.py command line.
-        For example, to change the exploration rate, try:
-            python pacman.py -p PacmanSarsaAgent -a epsilon=0.1
-
-        alpha    - learning rate
-        epsilon  - exploration rate
-        gamma    - discount factor
-        numTraining - number of training episodes, i.e. no learning after these many episodes
-        """
-        args['epsilon'] = epsilon
-        args['gamma'] = gamma
-        args['alpha'] = alpha
-        args['numTraining'] = numTraining
-        self.index = 0  # This is always Pacman
-        SarsaAgent.__init__(self, **args)
-
-    def getAction(self, state):
-        """
-        Simply calls the getAction method of SarsaAgent and then
-        informs parent of action for Pacman.  Do not change or remove this
-        method.
-        """
-        action = SarsaAgent.getAction(self, state)
-        self.doAction(state, action)
-        return action
-
-
-class ApproximateSarsaAgent(PacmanSarsaAgent):
-    """
-       ApproximateSarsaAgent
-       You should only have to overwrite getQValue
-       and update.  All other SarsaAgent functions
-       should work as is.
-    """
-
-    def __init__(self, extractor='ComplexExtractor', **args):
-        self.featExtractor = util.lookup(extractor, globals())()
-        PacmanSarsaAgent.__init__(self, **args)
-        self.weights = util.Counter()
 
     def getWeights(self):
         return self.weights
 
     def getQValue(self, state, action):
-        """
-          Should return Q(state,action) = w * featureVector
-          where * is the dotProduct operator
-        """
-        "*** YOUR CODE HERE ***"
         return self.weights * self.featExtractor.getFeatures(state, action)
 
     def update(self, state, action, nextState, reward):
-        """
-           Should update your weights based on transition
-        """
-        "*** YOUR CODE HERE ***"
         nextAction = self.getAction(nextState)
         if nextAction is None:
             nextQVal = 0.
@@ -204,7 +90,6 @@ class ApproximateSarsaAgent(PacmanSarsaAgent):
             nextQVal = self.getQValue(nextState, nextAction)
         difference = reward + self.discount * nextQVal - self.getQValue(state, action)
         # print("{0:.2f}(diff) = {1:.2f}(reward) + {2:.2f}(discount) x {3:.2f}(nextQVal) - {4:.2f}(currentQVal)".format(difference, reward, self.discount, nextQVal, self.getQValue(state, action)))
-        # difference = reward + self.discount * self.computeValueFromQValues(nextState) - self.getQValue(state, action)
         features = self.featExtractor.getFeatures(state, action)
         for feature_key in features:
             if feature_key not in self.weights.keys():
@@ -213,11 +98,7 @@ class ApproximateSarsaAgent(PacmanSarsaAgent):
 
     def final(self, state):
         "Called at the end of each game."
-        # call the super-class final method
         PacmanSarsaAgent.final(self, state)
-
-        # did we finish training?
         if self.episodesSoFar == self.numTraining:
-            # you might want to print your weights here for debugging
-            "*** YOUR CODE HERE ***"
+            pass
             # print(self.weights)
